3c7ad3ddb26417d07d7460db423ccbc76599039a
Dave Stanton <dstanton@dstanton-ltm.internal.salesforce.com>
2019-06-25 07:55:05 -0400

    Fixed semantics. Added a second pge.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,13 +10,9 @@
 	<title>Dave Stanton: Home</title>
 </svelte:head>
 
-<header>
-	<h1>My name is Dave Stanton</h1>
-</header>
+<h1>My name is Dave Stanton</h1>
 
-<main>
-	<p>I am a software architect and technical coach. I work with teams to integrate quality, scalability, security, and accessibility into enterprise mobile and cloud projects. I earned a Ph.D. from the University of Florida by researching the behavioral and cognitive effects of interface design.</p>
-</main>
+<p>I am a software architect and technical coach. I work with teams to integrate quality, scalability, security, and accessibility into enterprise mobile and cloud projects. I earned a Ph.D. from the University of Florida by researching the behavioral and cognitive effects of interface design.</p>
 
 <footer>
     <p>&copy; {year}</p>
